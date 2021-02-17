7e758e77ee1249f3601674f90b348db82110895b
Pablo Oliveira Mesquita <pablo_omesquita@hotmail.com>
2021-02-17 17:06:08 -0300

    CRUD

--- back-end/app.js
+++ back-end/app.js
@@ -7,6 +7,7 @@ app.set('view engine', 'ejs')
 
 app.use(cors())
 
+// ############################ USUÁRIOS ########################
 app.get("/usuarios", (req, res, next)=>{
     const mysql =  require("mysql")
     const connection = mysql.createConnection({
@@ -15,14 +16,30 @@ app.get("/usuarios", (req, res, next)=>{
         password: '',
         database:'recodeprojeto'
     })
-
-        //res.render('posts.ejs', {dados:result})
         connection.query('SELECT * FROM usuarios', (error, result)=>{
             res.json({dados: result})
         })
 });
+app.post('/usuario/salvar',(req, res, next) =>{
 
+    var cadastro = req.body;
+    const mysql =  require("mysql")
+    const connection = mysql.createConnection({
+        host:'localhost',
+        user: 'root',
+        password: '',
+        database:'recodeprojeto'
+    })
+    
+    connection.query('INSERT INTO mutiroes SET?', cadastro, (error, result)=>{
+        console.log(error, result)
+    });
+
+})
+
+// ############################ MUTIROES########################
 app.get('/mutiroes', (req, res, next)=>{
+
     const mysql = require('mysql')
     const connection = mysql.createConnection({
         host: 'localhost',
@@ -34,6 +51,24 @@ app.get('/mutiroes', (req, res, next)=>{
         res.json({dados:result})
     })
 })
+app.post('/mutirao/salvar',(req, res, next) =>{
+    
+    const mysql = require('mysql')
+    var cadastro = req.body;
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user:'root',
+        password:'',
+        database:'recodeprojeto'
+    })
+
+    connection.query('INSERT INTO mutiroes SET?', cadastro, (error, result)=>{
+        console.log(error, result)
+    });
+
+})
+
+
 
 app.listen(1910, ()=>{
     console.log('O servidor subiu na porta 1910')
